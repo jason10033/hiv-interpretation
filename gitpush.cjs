4a431e6c425c4947dd9214608dcf8f4b9a01104c
Jason <jason10033@github.com>
2026-04-07 11:54:42 -0400

    Switch HashRouter to BrowserRouter for proper page-level analytics tracking

--- gitpush.cjs
+++ gitpush.cjs
@@ -19,7 +19,7 @@ function run(cmd) {
 run('config user.email "jason10033@github.com"')
 run('config user.name "Jason"')
 run('add -A')
-run('commit -m "Add Fathom analytics"')
+run('commit -m "Switch HashRouter to BrowserRouter for proper page-level analytics tracking"')
 run('push origin main')
 log.push('\n--- Done. Check https://hiv-interpretation.netlify.app ---')
 log.forEach(l => console.log(l))
